ee79432bdfafa483f08ca20ede12321253d8186d
jmattew <jacobdotmathew@gmail.com>
2026-04-07 14:00:59 -0700

    starting mlp makemore version

--- makemoremlp.py
+++ makemoremlp.py
@@ -1,1 +1,33 @@
+#this line helps us open up the names.txt file and then allows us to read what's in it and then put the results on new lines
+from typing import Any
+import torch
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+words = open('names.txt', 'r').read().splitlines()  
+
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i+1 for i,s in enumerate(chars)} # mapping of s to i in the 2d array, rows is s, columns is i
+stoi['.'] = 0
+N = torch.zeros((27,27),dtype=torch.int32) # pytorch automatically sets the data to be 32 bit floats, but we want to use integers as we are storing counts
+
+itos = {i:s for s,i in stoi.items()}
+
 #this file is for the multi-layer perceptron model version of the bigram language model
+
+block_size = 3 # context length, how many characters do we take in to predict the next character?
+X, Y = [], []
+
+for w in words[:5]:
+    print(w)
+    context = [0] * (block_size - 1)
+    for ch in w + '.':
+        ix = stoi[ch]
+        X.append(context)
+        Y.append(ix)
+
+        print(''.join(itos[i] for i in context), '--->', itos[ix])
+        context = context[1:] + [ix] # crop and append
+
+X = torch.tensor(X)
+Y = torch.tensor(Y)
