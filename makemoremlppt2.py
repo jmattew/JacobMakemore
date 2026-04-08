d8115d328b033762179531962ee935fa09b662a6
jmattew <jacobdotmathew@gmail.com>
2026-04-07 23:09:47 -0700

    starting to train model with set training data

--- makemoremlppt2.py
+++ makemoremlppt2.py
@@ -38,13 +38,68 @@ def build_dataset(words):
 
 random.seed(42)
 random.shuffle(words)
-n1 = int(0.8*len(words))
-n2 = int(0.9*len(words))
+n1 = int(0.8*len(words)) # 80% of the words are in our dataset for training
+n2 = int(0.9*len(words)) # 90% of the words are in our dataset for validation
 
-Xtr, Ytr = build_dataset(words[:n1])
-Xva, Yva = build_dataset(words[n1:n2])
-Xte, Yte = build_dataset(words[n2:])
+Xtr, Ytr = build_dataset(words[:n1]) # training data, about 80% of data
+Xva, Yva = build_dataset(words[n1:n2]) # validation data, about 10% of data
+Xte, Yte = build_dataset(words[n2:]) # test data, about 10% of data
 
-# print(Xtr.shape, Ytr.shape)
-# print(Xva.shape, Yva.shape)
-# print(Xte.shape, Yte.shape)
+#C is the character embedding matrix where each row is a learned vector for one symbol(one of 27 characters)
+#before training we initialize the character embedding matrix with random values, from a normal distribution with mean 0 and standard deviation 1
+#the entries of C must be floats as they are learned continuous values which we will use to compute dot products with the one hot encoded input to get logits
+
+#So X is what the model sees, Y is the correct next character that comes after the X data and the learned outputs live in parameters like C
+C = torch.randn((27,2), generator = g) # our lookup table, 27 rows for the 27 characters, 2 columns for the 2 hidden units
+
+# Now for Y, we have to pluck out the character we want and the actual probability we have for predicting it
+#prob[torch.arange(Y.shape[0]), Y]
+
+W1 = torch.randn((6,100), generator = g)
+b1 = torch.randn(100, generator = g)
+
+
+W2 = torch.randn((100,27), generator = g) 
+b2 = torch.randn(27, generator = g) 
+
+
+parameters = [C, W1, b1, W2, b2]
+
+for p in parameters:
+    p.requires_grad = True 
+
+#We want to find the best leanring rate for the model, so we will try different learning rates and see which one works best
+lre = torch.linspace(-3,0,1000) # learning rate exponent
+lrs = 10**lre # learning rate(10 to the power of learning rate exponent)
+
+lri = [] # to keep track of learning rates we've used
+lossi = [] # to keep track of losses we've seen
+
+for k in range(10000): # we go through 10 iterations to train the model
+
+    ix = torch.randint(0, Xtr.shape[0], (32,)) # use XTrain data, making minibatches of 32 to train the model all rather than using 32,000 datapoints
+    #FORWARD PASS
+    emb = C[Xtr[ix]] # use XTrain data, each row of emb is the embedding of the corresponding row of X, X[ix] is [32,3,2]
+    h = torch.tanh(emb.view(-1,6) @ W1 + b1)
+    logits = h @ W2 + b2 # logits are the unnormalized probabilities for each character
+    loss = F.cross_entropy(logits, Ytr[ix]) # cross_entropy function also calculates the loss with logits and Y tensor
+    #print(k, '   ', loss.item())
+
+    #BACKWARD PASS
+    for p in parameters:#initialize the gradients to 0
+        p.grad = None
+    loss.backward() # backpropgation
+
+    #Update Parameters
+    #lr = lrs[k]
+    lr = 0.1# we found that the best learning rate is about 0.1 based on tracking the stats for a more generalized model
+    # we decrease learning rate for the model to understand more specific patterns in later stages of training
+    for p in parameters:
+        p.data += -lr * p.grad # learning rate * gradients
+
+    #track stats
+    #lri.append(lr)
+    #lossi.append(loss.item())
+
+print(loss.item())
+
